e58f95736527ebe540953938d417c411dc955b21
Raj <rajkarri85@gmail.com>
2017-03-29 15:43:03 -0500

    Json conversions

---
+++ JsonConversions/JsonConversions/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Department.cs" company="XXXXXXX">
+// Copyright (c) XXXXXXX. All rights reserved
+// </copyright>
+//-----------------------------------------------------------------------
+namespace JsonConversions.Models
+{
+    /// <summary>
+    /// Department model
+    /// </summary>
+    public class Department
+    {
+        /// <summary>
+        /// Gets or sets identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of the department
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
